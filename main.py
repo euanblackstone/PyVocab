b3dc8e38c851c15b9fd89ebc3698d3d64f034df1
Euan <euan.blackstone@pridetronics.com>
2020-02-10 21:51:36 -0700

    Not working

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from PyDictionary import PyDictionary
 
 def main():
-    pass
+    dictionary = PyDictionary()
+    file = open("words.txt", 'r')
+    defdict = {}
+    for word in file.readlines():
+        tempword = word.strip('\n')
+        definition = dictionary.meaning(tempword)
+        defdict[tempword] = definition
+    file.close()
+    file = open("results.txt", "w")
+    for k, v in defdict.items():
+        tempstring = ("%s - %s" % (k, v))
+        file.write(tempstring)
 
 if __name__ == "__main__":
     main()
